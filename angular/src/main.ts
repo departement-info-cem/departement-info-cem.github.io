473578e240ff040770653e8f9c154a7e340f1ddd
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2026-01-23 16:07:46 -0500

    Upgrade Angular dependencies to version 21.1.1 and update TypeScript to 5.9.3; add zone change detection in main.ts

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
@@ -29,7 +29,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
+    provideZoneChangeDetection(),importProvidersFrom(
       BrowserModule,
       MatButtonModule,
       MatDialogModule,
